a39a7fff8c383f18b93d7afd50eec64fa8a1b4f7
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-06-10 17:15:39 -0400

    Update index.tsx

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -59,8 +59,8 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+        headline=" Triple x Saints Burn 2 Earn."
+        description=“AN OBJECT IN MOTION STAYS IN MOTION. OUR HEALTH IS WHAT KEEPS US IN MOTION, SO EARN WHILE BEING HEALTHY”
       />
       <Grid layout="B" variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
